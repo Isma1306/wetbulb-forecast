7b07af8aeb75acf1eae1ffecb46b252dfa4d6644
Isma1306 <isma1306@gmail.com>
2023-08-26 17:45:59 +0200

    improve styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -63,11 +63,13 @@ const App: Component = () => {
         </Show>
       </div>
       <footer>
-        Thanks to <a href='https://open-meteo.com/'>Open-Meteo</a> for the data!
-        <span>
-          {' '}
-          - Make with love by <a href='https://github.com/Isma1306'>Isma</a>
-        </span>
+        <p>
+          Thanks to <a href='https://open-meteo.com/'>Open-Meteo</a> for the data!
+          <span>
+            {' '}
+            - Make with love by <a href='https://github.com/Isma1306'>Isma</a>
+          </span>
+        </p>
       </footer>
     </div>
   )
