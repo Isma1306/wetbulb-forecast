83fd270abd7ad237d02f827bdacff68f0b0f443c
Isma1306 <isma1306@gmail.com>
2023-09-04 18:19:53 +0200

    modified the url to separate FE from the BE

--- backend/main.go
+++ backend/main.go
@@ -12,7 +12,7 @@ func main() {
 	r := gin.Default()
 	r.SetTrustedProxies(nil)
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:3000"}
+	config.AllowOrigins = []string{"https://wetbulb-8f6k.onrender.com"}
 	config.AllowMethods = []string{"GET"}
 	config.AllowHeaders = []string{"*"}
 	// r.Use(cors.New(config))
