496b2cbeee9f6d6b6adcf055012b4cfa1d2e1ad7
Isma1306 <isma1306@gmail.com>
2023-11-26 17:10:16 +0100

    Added env variables

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+	"os"
 	"wetBulb/controllers"
 
 	"github.com/gin-contrib/cors"
@@ -11,22 +13,21 @@ import (
 func main() {
 	r := gin.Default()
 	r.SetTrustedProxies(nil)
+	FEURL := os.Getenv("FEURL")
+	if FEURL == "" {
+		log.Fatal("missing enviroment variables!")
+	}
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"https://wetbulb-8f6k.onrender.com"}
-	// config.AllowOrigins = []string{"*"}
+	config.AllowOrigins = []string{FEURL}
 	config.AllowMethods = []string{"GET"}
 	config.AllowHeaders = []string{"*"}
-	// r.Use(cors.New(config))
 	r.Use(gzip.Gzip(gzip.DefaultCompression))
-	// r.GET("/", func(c *gin.Context) {
-	// 	c.Request.URL.Path = "/home"
-	// 	r.HandleContext(c)
-	// })
+
 	api := r.Group("/api")
 	api.Use(cors.New(config))
 	api.GET("/forecast", controllers.GetForecast)
 	api.GET("/location", controllers.GetLocation)
-	// r.StaticFS("/home", http.Dir("./public"))
+
 	r.Run()
 
 }
